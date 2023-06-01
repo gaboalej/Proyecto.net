3af457832310f051c67bbb6df4e2fcbc38316105
gaboalej <gabobnlk@gmail.com>
2023-06-01 14:10:43 -0300

    seteando los metodos getset con su nombres

--- TPFinalNivel2_Pelozo/Dominio/MetodosGetSet.cs
+++ TPFinalNivel2_Pelozo/Dominio/MetodosGetSet.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class MetodosGetSet
+    public class MetodosGetSet
     {
+        public int Id { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int IdMarca { get; set; }
+
+        public int IdCategoria {get; set;}
+
+        public string ImagenUrl { get; set; }
+
+        public int Precio { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
